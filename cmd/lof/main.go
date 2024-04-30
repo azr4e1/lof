88174f523fe05c7559d69e28b895d58d0d6994e0
Lorenzo Drumond <drumondlorenzo@posteo.net>
2024-04-30 21:14:33 +0100

    fix(actions): fix main file format

--- cmd/lof/main.go
+++ cmd/lof/main.go
@@ -1,21 +1,9 @@
 package main
 
 import (
-	"fmt"
-	"os"
-
-	"github.com/azr4e1/lof"
+	"github.com/azr4e1/lof/cmdline"
 )
 
 func main() {
-	node, err := lof.GetTree()
-	if err != nil {
-		os.Exit(1)
-	}
-
-	data, err := lof.OutputWindows(node, func(bn *lof.BaseNode) bool {
-		return bn.IsTrueWindow()
-	}).ToJSON()
-
-	fmt.Println(string(data))
+	cmdline.Main()
 }
